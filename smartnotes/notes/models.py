d6c0876b8cbba066296a007bce094f9c31ded809
sujalbahing <sujal.rai147@gmail.com>
2024-07-16 23:35:59 +0545

    notes app added

--- smartnotes/notes/models.py
+++ smartnotes/notes/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 
 
+class Notes(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    
--- smartnotes/notes/views.py
+++ smartnotes/notes/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import Http404  
+from .models import Notes
 
-# Create your views here.
+
+def list(request):
+    all_notes = Notes.objects.all()
+    return render(request, 'notes/notes.html',{'notes':all_notes})
+
+
+def detail(request,pk):
+    try:
+        note = Notes.objects.get(pk=pk)
+    except Notes.DoesNotExist:
+        raise Http404("Note does not exist")
+    return render(request, 'notes/notes_details.html',{'note':note})
