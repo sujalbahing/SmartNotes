7b4a366deeaa60e4ce2e746973dc288cd0c499d5
sujalbahing <sujal.rai147@gmail.com>
2024-07-18 00:12:27 +0545

    forms.py added

--- smartnotes/notes/views.py
+++ smartnotes/notes/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.http import Http404  
 from .models import Notes
+from django.views.generic import CreateView, DetailView, ListView
+from .forms import NotesForm
+class NotesCreateView(CreateView):
+    model = Notes
+    success_url = '/notes'  # this is the URL where the user will be redirected after a successful form submission.
+    form_class = NotesForm
 
-
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes.html',{'notes':all_notes})
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = 'notes'
+    template_name = 'notes/notes_list.html'
+    
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = 'note'  # this is the name of the variable in the template that will receive the note object.
 
 
 def detail(request,pk):
@@ -13,4 +23,4 @@ def detail(request,pk):
         note = Notes.objects.get(pk=pk)
     except Notes.DoesNotExist:
         raise Http404("Note does not exist")
-    return render(request, 'notes/notes_details.html',{'note':note})
+    return render(request, 'notes/detail.html',{'note':note})
