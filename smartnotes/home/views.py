c5f9463a6727e74edc44e296a1a7a371001fdeac
sujalbahing <sujal.rai147@gmail.com>
2024-07-18 15:07:27 +0545

    authentication login logout signup added

--- smartnotes/home/views.py
+++ smartnotes/home/views.py
@@ -3,9 +3,29 @@ from django.http import HttpResponse
 from datetime import datetime
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
-
-
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 # Class based views
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'home/register.html'
+    success_url = '/notes'
+    
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes.list')
+        return super().get(request, *args, **kwargs)
+
+class LoginInterfaceView(LoginView):
+    template_name = 'home/login.html'
+    
+class LogoutInterfaceView(LogoutView):
+    template_name = 'home/logout.html'
+    
 class HomeView(TemplateView):
     template_name = 'home/welcome.html'
     extra_context = {'today':datetime.today()}
@@ -13,5 +33,4 @@ class HomeView(TemplateView):
 class AuthorizedView(LoginRequiredMixin, TemplateView):
     template_name = 'home/authorized.html'
     login_url='/admin'
-
 
--- smartnotes/notes/views.py
+++ smartnotes/notes/views.py
@@ -1,24 +1,47 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.forms import BaseModelForm
 from django.shortcuts import render
-from django.http import Http404  
+from django.http import Http404, HttpResponse, HttpResponseRedirect  
 from .models import Notes
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from .forms import NotesForm
+from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class NotesUpdateView(UpdateView):
+class NotesDeleteView(LoginRequiredMixin,DeleteView):
+    model = Notes
+    success_url = '/notes' 
+    template_name = 'notes/notes_delete.html'  # this is the template that will be used to display the delete confirmation page.
+    login_url = '/admin'
+
+class NotesUpdateView(LoginRequiredMixin,UpdateView):
     model = Notes
     success_url = '/notes' 
     form_class = NotesForm
+    login_url = '/admin'
 
-class NotesCreateView(CreateView):
+class NotesCreateView(LoginRequiredMixin,CreateView):
     model = Notes
     success_url = '/notes'  # this is the URL where the user will be redirected after a successful form submission.
     form_class = NotesForm
-
-class NotesListView(ListView):
+    login_url = '/admin'
+    
+    def form_valid(self, form):
+        self.object = form.save(commit=False )
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url)
+        
+class NotesListView(LoginRequiredMixin,ListView):
     model = Notes
     context_object_name = 'notes'
     template_name = 'notes/notes_list.html'
+    login_url = '/admin'
+    
+    def get_queryset(self):
+        return self.request.user.notes.all()
     
 class NotesDetailView(DetailView):
     model = Notes
